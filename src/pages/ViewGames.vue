6dd485562deff728c60a8f748040759cc2e25293
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-28 19:47:40 -0400

    finished homework

--- src/pages/ViewGames.vue
+++ src/pages/ViewGames.vue
@@ -1,18 +1,80 @@
 <template>
-  <div className="container-grid"></div>
+   <h1 class="game-names">Games:</h1>
+   <label class="menu" for="rating">Sort By: </label>
+<select class="drop" name="ratings" @change="getSorted($event)">
+    <option id="drop" value="ascending">Highest Rating</option>
+  <option id="drop" value="descending">Lowest Rating</option>
+  
+</select>
+  <div class="container-grid" v-if="games"></div>
+  <div v-for="game in games" :key="game.id">
+   <h3 class="game-names b">{{ game.name }}</h3>
+   <div class="game-names">Rating: {{ game.rating }}</div>
+  </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const API_KEY = process.env.VUE_APP_RAWG_KEY;
   export default {
     name: 'ViewGames',
     data: () => ({
-      games: []
+      genreId: null,
+      games: [],
+      ratings: null
     }),
-    mounted() {},
+    mounted: function () {
+      this.genreId = parseInt(this.$route.params.genre_id)
+      this.getGamesByGenre()
+    },
     methods: {
       async getGamesByGenre() {
-        // Get Genre Id here
+         const res = await axios.get(`https://api.rawg.io/api/games?genres=${this.genreId}&key=${API_KEY}`)
+          this.games = res.data.results
+          console.log(res.data.results)
+      },
+      getSorted(type) {
+        console.log(type.target.value)
+        if (type.target.value === 'descending' ) {
+        this.games.sort(function(a, b) {
+         return a.rating - b.rating;
+       })
+      } else {
+         this.games.sort(function(a, b) {
+         return b.rating - a.rating;
+       })
+      }
       }
     }
   }
 </script>
+
+<style>
+.container-grid {
+  text-align: center;
+  font-size: 25px;
+}
+.game-names {
+  margin: 25px 45px;
+  padding: 10px;
+  background-color: rgb(98, 100, 78);
+}
+
+.menu {
+  padding: 15px;
+  margin: 25px 45px;
+  font-size: 25px;
+ 
+}
+
+.drop {
+  padding: 10px;
+  font-size: 20px;
+  
+}
+
+.b {
+  background-color: rgb(98, 100, 78);
+  padding: 10px;
+}
+</style>
