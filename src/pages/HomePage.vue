078043320b79c3d37aad2afae871e99d10c5ed08
Mark Harmon <neothej1sec@gmail.com>
2022-04-28 11:34:02 -0500

    finished step 3

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,23 +8,43 @@
 
     <div class="genres">
       <h2>Genres</h2>
-      <section class="container-grid"></section>
+      <section class="container-grid">
+          <GenreCard :key="genre.id" v-for="genre in genres">
+            <h2>{{genre.name}}</h2>
+            <img :src="genre.image_background" />
+          </GenreCard>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+//import GenreCard from '../components/GenreCard.vue'
+const API_KEY = process.env.VUE_APP_API_KEY
+//const SEARCH_URL = `https://api.rawg.io/api/games?key=${API_KEY}&search=${searchQuery}`
+const GENRES_URL = `https://api.rawg.io/api/genres?key=${API_KEY}`
+//const GAME_DETAIL_URL = `https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`
+//const DLC_URL = `https://api.rawg.io/api/games/${gameId}/additions?key=${API_KEY}`
+
   export default {
     name: 'HomePage',
+    components: {
+      //GenreCard
+    },
     data: () => ({
       genres: [],
       searchQuery: '',
       searchResults: [],
       searched: false
     }),
-    mounted() {},
+    mounted: function(){this.getGenres()},
     methods: {
-      async getGenres() {},
+      async getGenres() {
+        const res = await axios.get(GENRES_URL)
+        this.genres = res.data.results
+        console.log(this.genres, "RES")
+      },
       async getSearchResults(e) {
         e.preventDefault()
       },
@@ -37,3 +57,10 @@
     }
   }
 </script>
+
+<style scoped>
+img{
+  max-width: 30vw;
+  max-height: 70vw;
+}
+</style>
