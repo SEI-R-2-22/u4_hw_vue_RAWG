ad954f950e35ae176b5de1c213962ef6c3572dc2
Ethan Nitz <etnitz@gmail.com>
2022-04-28 20:21:54 -0500

    finished HW

--- src/pages/GameDetails.vue
+++ src/pages/GameDetails.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="game-content">
+  <div class="game-content" v-if="gameDetails">
     <section class="image-container">
-      <div></div>
+      <button @click="backButton">Go Back</button>
+      <div>
+        <img :src="gameDetails.background_image_additional" />
+      </div>
     </section>
     <section class="details">
       <div class="flex-row space"></div>
       <div>
-        <h3></h3>
+        <h3>{{ gameDetails.name }}</h3>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const API_KEY = process.env.VUE_APP_RAWG_API
   export default {
     name: 'GameDetails',
     data: () => ({
       gameDetails: null
     }),
-    mounted() {},
+    mounted: function() {
+      this.getGameDetails()
+    },
     methods: {
       async getGameDetails() {
-        // Get game id from router here
+        let gameId = parseInt(this.$route.params.game_id)
+        const res = await axios.get(`https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`)
+        this.gameDetails = res.data.results
+      },
+      backButton() {
+        this.$router.go(-1)
       }
     }
   }
--- src/pages/ViewGames.vue
+++ src/pages/ViewGames.vue
@@ -1,17 +1,47 @@
 <template>
-  <div className="container-grid"></div>
+  <div>
+    <label>SORT:</label>
+    <select @click="sortGames">
+      <option>SORT</option>
+      <option>ASCEND</option>
+      <option>DESCEND</option>
+    </select>
+  </div>
+  <div className="container-grid">
+    <div v-for="game in games" :key="game.id">
+      <h3>{{ game.name }}</h3>
+      <h6>{{ game.rating }}</h6>
+    </div>
+  </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const API_KEY = process.env.VUE_APP_RAWG_API
   export default {
     name: 'ViewGames',
     data: () => ({
       games: []
     }),
-    mounted() {},
+    mounted() {
+      this.getGamesByGenre()
+    },
     methods: {
       async getGamesByGenre() {
-        // Get Genre Id here
+        let genreId = parseInt(this.$route.params.genre_id)
+        const res = await axios.get(`https://api.rawg.io/api/games?genres=${genreId}&key=${API_KEY}`)
+        this.games = res.data.results
+      },
+      sortGames(e) {
+        if(e.target.value === "ASCEND") {
+          this.games = this.games.sort((a,b) => {
+            return a.rating - b.rating
+          })
+        } else if (e.target.value === "DESCEND") {
+          this.games = this.games.sort((a,b) => {
+            return b.rating - a.rating
+          })
+        }
       }
     }
   }
