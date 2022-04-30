5da5d5ca2e34d6fa0016700d0088ee2fb78c4c45
Aqsa Saleheene <angieclrk21@gmail.com>
2022-04-30 19:46:48 -0400

    HW Done

--- src/pages/GameDetails.vue
+++ src/pages/GameDetails.vue
@@ -1,28 +1,48 @@
 <template>
   <div class="game-content">
+    <div v-if="gameDetails" class="game-content">
     <section class="image-container">
       <div></div>
+      <div>
+        <img :src="gameDetails.background_image"/>
+      </div>
     </section>
     <section class="details">
       <div class="flex-row space"></div>
       <div>
         <h3></h3>
+        <h3>
+          {{gameDetails.name}}
+        </h3>
+        <p>{{gameDetails.description_raw}}</p>
       </div>
+      <router-link to="/"><button>Back</button></router-link>
     </section>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'GameDetails',
-    data: () => ({
-      gameDetails: null
-    }),
-    mounted() {},
-    methods: {
-      async getGameDetails() {
-        // Get game id from router here
-      }
-    }
-  }
+import axios from "axios";
+const API_KEY = process.env.VUE_APP_RAWG_KEY;
+
+export default {
+  name: "GameDetails",
+  data: () => ({
+    gameDetails: null,
+  }),
+  mounted() {},
+  mounted: async function () {
+    await this.getGameDetails();
+  },
+  methods: {
+    async getGameDetails() {
+      // Get game id from router here
+      let gameId = parseInt(this.$route.params.game_id);
+      const res = await axios.get(
+        `https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`
+      );
+      this.gameDetails = res.data;
+    },
+  },
+};
 </script>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,19 @@
-import { createWebHistory, createRouter } from "vue-router"
-import HomePage from './pages/HomePage'
-import GameDetails from './pages/GameDetails'
-import ViewGames from './pages/ViewGames'
-import AboutPage from './pages/AboutPage'
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "./pages/HomePage";
+import GameDetails from "./pages/GameDetails";
+import ViewGames from "./pages/ViewGames";
+import AboutPage from "./pages/AboutPage";
 
-const routes = []
+const routes = [];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes: [
+    { path: "/", component: HomePage, name: "Homepage" },
+    { path: "/details/:game_id", component: GameDetails, name: "GameDetails" },
+    { path: "/about", component: AboutPage, name: "AboutPage" },
+    { path: "/games/:genre_id", component: ViewGames, name: "ViewGames" },
+  ],
+});
 
-export default router
+export default router;
