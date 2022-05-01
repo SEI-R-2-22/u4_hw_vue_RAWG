5e8520cece5cb44f5320114925aaf9996f019fbc
Mark Ryan <markrryan99@gmail.com>
2022-05-01 09:40:29 -0700

    I think I finsihed everything

--- src/pages/GameDetails.vue
+++ src/pages/GameDetails.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="game-content">
     <section class="image-container">
-      <div></div>
+      <div>
+        <img :src='gameDetails.background_image' />
+      </div>
     </section>
     <section class="details">
       <div class="flex-row space"></div>
       <div>
-        <h3></h3>
+        <h3>{{ gameDetails.name }}</h3>
+        <h4>{{ gameDetails.rating }}</h4>
+        <button @click='home'>Home</button>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+const BASE_URL = 'https://api.rawg.io'
+
   export default {
     name: 'GameDetails',
     data: () => ({
-      gameDetails: null
+      gameDetails: [],
+      gameId: null
     }),
-    mounted() {},
+    mounted() {
+      this.gameId = parseInt(this.$route.params.game_id)
+       this.getGameDetails()
+    },
     methods: {
       async getGameDetails() {
-        // Get game id from router here
+        const res = await axios.get(
+          `${BASE_URL}/api/games/${this.$route.params.game_id}?key=9d413701ecfd4c75ae68781db863731a`
+          )
+        this.gameDetails = res.data
+        console.log(res.data)
+      },
+      home() {
+        this.$router.push('/')
       }
     }
   }
