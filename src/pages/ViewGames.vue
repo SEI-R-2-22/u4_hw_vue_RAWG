209be7352f5071306c189ac04dddc58c0549fce7
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-01 23:14:12 -0400

    finished rest of ViewGames.vue

--- src/pages/ViewGames.vue
+++ src/pages/ViewGames.vue
@@ -1,25 +1,28 @@
 <template>
-  <div className="container-grid" v-if="games">
-      <GameCard
-        v-for="game in games"
-        :key="game.id"
-        :game="game"
-        @click="() => selectGame(game.id)"
-      />
+  <h1 class="header">Games</h1>
+  <label class="sort" for="rating">Sort By: </label>
+  <select class="sort-box" name="ratings" @change="getSorted($event)">
+    <option value="ascending">Best Rating</option>
+    <option value="descending">Worst Rating</option>
+  </select>
+  <div class="container-grid" v-if="games"></div>
+  <div class= "flex-box" v-for="game in games" :key="game.id">
+    <div class = "card game-card">
+      <h3>{{ game.name }}</h3>
+      <div class="image-wrapper">
+        <img :src="game.background_image" />
+      </div>
+      <p>Rating: {{ game.rating }}</p>
     </div>
-    <div v-else>Loading Games...</div>
+  </div>
 </template>
 
 <script>
 const API_KEY = process.env.VUE_APP_RAWG_KEY;
 import axios from "axios";
-import GameCard from "../components/GameCard.vue";
 
 export default {
   name: "ViewGames",
-  components: {
-    GameCard,
-  },
   data: () => ({
     games: [],
   }),
@@ -33,17 +36,37 @@ export default {
       );
       this.games = res.data.results;
     },
-    // getSorted(type) {
-    //   if (type.target.value === "descending") {
-    //     this.games.sort(function (a, b) {
-    //       return a.rating - b.rating;
-    //     });
-    //   } else {
-    //     this.games.sort(function (a, b) {
-    //       return b.rating - a.rating;
-    //     });
-    //   }
-    // },
+    getSorted(type) {
+      if (type.target.value === "descending") {
+        this.games.sort(function (a, b) {
+          return a.rating - b.rating;
+        });
+      } else {
+        this.games.sort(function (a, b) {
+          return b.rating - a.rating;
+        });
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+.header {
+  text-align: center;
+}
+
+.sort {
+  padding-right: 55%;
+  padding-left: 45%;
+}
+
+.sort-box {
+  margin-left: 45%;
+}
+
+.flex-box {
+  display: flex;
+  justify-content: center;
+}
+</style>
